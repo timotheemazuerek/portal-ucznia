c618589edbca1606e5614f4a86f0334d4e4ec9cd
timotheemazuerek <timothy.mazureq@icloud.com>
2025-06-30 14:26:10 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyBvegeEvSxBlx-1UUhD_PIywpjW3TlL8So",
+    authDomain: "subos-1-0-beta-10-pre-release.firebaseapp.com",
+    projectId: "subos-1-0-beta-10-pre-release",
+    storageBucket: "subos-1-0-beta-10-pre-release.appspot.com",
+    messagingSenderId: "914402204178",
+    appId: "1:914402204178:web:84e7accc42e5e1711096cb",
+    measurementId: "G-B06SXV7MKR"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
